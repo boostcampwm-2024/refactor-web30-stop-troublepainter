e4d7693d3cd562ff54a8e71383327c38ae3f0f43
AEU <aeuplayground@gmail.com>
2025-01-09 14:38:30 +0900

    test: Create drawingTest hook

--- client/src/hooks/test/drawingTest.ts
+++ client/src/hooks/test/drawingTest.ts
@@ -1,4 +1,5 @@
 import { MutableRefObject, useEffect, useRef } from 'react';
+import { Point } from '@troublepainter/core';
 import { coordinates } from './drawingTestPoints';
 
 function* getOnePoint(): Generator<
@@ -14,11 +15,18 @@ function* getOnePoint(): Generator<
   }
 }
 
-export const drawingTest = (workerRef: MutableRefObject<Worker | undefined>) => {
+export const drawingTest = (pointReceiver: MutableRefObject<Worker | Point[] | undefined>, num: number) => {
   const timer = useRef<NodeJS.Timeout>();
   const startTime = performance.now();
 
+  const printResult = (finishTime: DOMHighResTimeStamp) => {
+    console.log('startTIme : ', startTime, 'endTime : ', finishTime);
+    console.log('total : ', finishTime - startTime);
+  };
+
   useEffect(() => {
+    coordinates.length = num;
+    coordinates.push({ x: -1, y: -1 });
     const generator = getOnePoint();
 
     timer.current = setInterval(() => {
@@ -27,8 +35,16 @@ export const drawingTest = (workerRef: MutableRefObject<Worker | undefined>) => 
         clearInterval(timer.current);
         return;
       }
-      if (workerRef.current) {
-        workerRef.current.postMessage({ type: 'draw', value: { point: points } });
+      if (!pointReceiver.current) return;
+      if ('postMessage' in pointReceiver.current)
+        pointReceiver.current.postMessage({ type: 'draw', value: { point: points } });
+      else {
+        pointReceiver.current.push(points);
+
+        if (points.x == -1) {
+          console.log('Worker has signaled the end.');
+          printResult(performance.now());
+        }
       }
     }, 16);
 
@@ -37,9 +53,5 @@ export const drawingTest = (workerRef: MutableRefObject<Worker | undefined>) => 
     };
   }, []);
 
-  const printResult = (finishTime: DOMHighResTimeStamp) => {
-    console.log('startTIme : ', startTime, 'endTime : ', finishTime);
-    console.log('total : ', finishTime - startTime);
-  };
   return printResult;
 };
