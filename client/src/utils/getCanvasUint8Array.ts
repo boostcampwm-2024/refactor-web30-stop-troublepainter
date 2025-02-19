34d8e242afed19371779f72c47cc197da033359b
choiseona <tjsdk617@naver.com>
2025-02-19 16:27:23 +0900

    refactor: Convert canvas image to WebP format to optimize server traffic

--- client/src/utils/getCanvasUint8Array.ts
+++ client/src/utils/getCanvasUint8Array.ts
@@ -1,15 +1,23 @@
 import { RefObject } from 'react';
 
-export async function getCanvasUint8Array(canvasRef: RefObject<HTMLCanvasElement>) {
+type Format = 'image/png' | 'image/jpeg' | 'image/webp';
+
+export async function getCanvasUint8Array(canvasRef: RefObject<HTMLCanvasElement>, format: Format, quality?: number) {
   const canvas = canvasRef.current;
   if (!canvas) return;
-  const blob = await new Promise<Blob>((resolve) => {
-    canvas.toBlob((blob) => {
-      if (!blob) {
-        throw new Error('Failed to create blob from canvas');
-      }
-      resolve(blob);
-    });
+
+  const blob = await new Promise<Blob>((resolve, reject) => {
+    canvas.toBlob(
+      (blob) => {
+        if (!blob) {
+          reject(new Error('Failed to create blob from canvas'));
+          return;
+        }
+        resolve(blob);
+      },
+      format,
+      quality,
+    );
   });
 
   const arrayBuffer = await blob.arrayBuffer();
